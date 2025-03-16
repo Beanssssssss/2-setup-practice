0b13da861693a1fb328649341d2d5790a43402b3
JONGBEEN KU <bean4102@naver.com>
2025-03-16 15:32:18 +0900

    [feat] second commit

--- JONGBEEN/example.py
+++ JONGBEEN/example.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 # Get the API key from the environment variable
 openai_api_key = os.getenv("OPENAI_API_KEY")
-hubspot_api_key = os.getenv("HUBSPOT_API_KEY")
+hugginface__api_key = os.getenv("HUGGING_API_KEY")
 # Print the API key
 print(openai_api_key)
